d7966efe626a11ad1f59c1b3bfd3ede3ebc0d151
Pelle Nilsen <105052765+PelleNIlsen@users.noreply.github.com>
2023-05-22 14:56:42 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+    $("#btnRoll").click(function() {
+        let total = 0;
+        let output = '';
+        $("#dice-rolls").html('');
+        $("#sum").html('');
+
+        let diceIntervals = [];
+
+        for (let i = 0; i < $("#numNum").val(); i++) {
+            const randNum = Math.floor(Math.random() * $("#diceSides").val()) + 1;
+            output = '<div class="col-sm-4 mt-4"><div class="card"><div id="dice-' + i + '" class="card-body text-center">';
+            output += 'Dice ' + (i + 1) + ': <b>0</b>';
+            output += '</div></div></div>';
+            $("#dice-rolls").append(output);
+
+            diceIntervals[i] = setInterval(function() {
+                $("#dice-" + i).html('Roll ' + (i + 1) + ': <b>' + (Math.floor(Math.random() * $("#diceSides").val()) + 1) + '</b>');
+            }, 50);
+        }
+
+        let sumInterval = setInterval(function() {
+            $("#sum").html('Sum: <b>' + (Math.floor(Math.random() * ($("#diceSides").val() * $("#numNum").val())) + 1) + '</b>');
+        }, 50);
+
+        setTimeout(function() {
+            for (let i = 0; i < $("#numNum").val(); i++) {
+                let randNum = Math.floor(Math.random() * $("#diceSides").val()) + 1;
+                $("#dice-" + i).html('Roll ' + (i + 1) + ': <b>' + randNum + '</b>');
+                clearInterval(diceIntervals[i]);
+                total += randNum;
+            }
+
+            $("#sum").html('Sum: <b>' + total + '</b>');
+            clearInterval(sumInterval);
+        }, 2000); 
+    });
+});
